d01a58b77aa9ab602aea709b703991a150ea2e2e
Hofsep <hofsep_z89@hotmail.com>
2015-02-18 12:15:22 +0100

    modified repo

--- BinaryTree/src/vraag1/AbstractTree.java
+++ BinaryTree/src/vraag1/AbstractTree.java
@@ -1,61 +1,26 @@
 package vraag1;
 
-import java.util.Iterator;
-
-public class AbstractTree<E extends Comparable<E>> implements Tree<E> {
-
-	@Override
-	public boolean search(E e) {
-		// TODO Auto-generated method stub
-		return false;
+public abstract class AbstractTree<E extends Comparable<E>> implements Tree<E> {
+	
+	@Override /** Inorder traversal from the root*/
+	public void inorder(){
 	}
-
-	@Override
-	public boolean insert(E e) {
-		// TODO Auto-generated method stub
-		return false;
+	
+	@Override /** Preorder traversal from the root */
+	public void preorder(){
 	}
-
-	@Override
-	public boolean delete(E e) {
-		// TODO Auto-generated method stub
-		return false;
+	
+	@Override /** Postorder traversal from the root */
+	public void postorder(){
 	}
-
-	@Override
-	public void inOrder() {
-		// TODO Auto-generated method stub
-		
+	
+	@Override /** Return true if the tree is empty */
+	public boolean isEmpty(){
+		return getSize() == 0;
 	}
-
-	@Override
-	public void preOrder() {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void postOrder() {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public int getSize() {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-	@Override
-	public boolean isEmpty() {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public Iterator<E> iterator() {
-		// TODO Auto-generated method stub
+	
+	@Override /** Return an iterator for the tree */
+	public java.util.Iterator<E> iterator(){
 		return null;
 	}
-
 }
--- BinaryTree/src/vraag1/Tree.java
+++ BinaryTree/src/vraag1/Tree.java
@@ -1,34 +1,36 @@
 package vraag1;
 
 public interface Tree<E extends Comparable<E>> extends Iterable<E> {
-	
-	/* Return true if the element is in the tree*/
+	/**
+	 * Return true if the element is iun the tree
+	 */
 	public boolean search(E e);
 	
-	/* Insert element e into the binary search tree.
-	 * Return true if the element is inserted successfully.*/
+	/**
+	 * Insert element e into the binary search tree.
+	 * Return true if the element is inserted successfully
+	 */
 	public boolean insert(E e);
 	
-	/* Delete the specified element from the tree.
-	 * Return true if the element is deleted succesfully. */
+	/** Delete the specified element from the tree.
+	 * Return true if the element is deleted successfully. */
 	public boolean delete(E e);
 	
-	/* Inorder traversal from the root*/
-	public void inOrder();
+	/** Inorder traversal from the root*/
+	public void inorder();
 	
-	/* Inorder traversal from the root*/
-	public void preOrder();
+	/** Postorder traversal from the root */
+	public void postorder();
+	/** Preorder traversal from the root */
+	public void preorder();
 	
-	/* Postorder traversal from the root*/
-	public void postOrder();
-	
-	/* Get the number of nodes in the tree*/
+	/** Get the number of nodes in the tree */
 	public int getSize();
 	
-	/* Return true if the tree is empty*/
+	/** Return true if the tree is empty */
 	public boolean isEmpty();
 	
-	/* Return an iterator to traverse elements in the tree*/
-	public java.util.Iterator<E> iterator();	
+	/** Return an iterator to traverse elements in the tree */
+	public java.util.Iterator<E> iterator();
 
 }
