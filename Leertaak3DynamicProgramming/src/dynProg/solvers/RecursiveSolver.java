b959b950ef71083cf7ab8e78164f3167395dd231
Hofsep <hofsep_z89@hotmail.com>
2015-03-08 00:41:42 +0100

    created missing classes. LETOP import Junit

---
+++ Leertaak3DynamicProgramming/src/dynProg/solvers/RecursiveSolver.java
@@ -0,0 +1,13 @@
+package dynProg.solvers;
+
+import dynProg.Solver;
+
+public class RecursiveSolver implements Solver {
+
+	@Override
+	public boolean solve(int[] numbers, int sum) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
