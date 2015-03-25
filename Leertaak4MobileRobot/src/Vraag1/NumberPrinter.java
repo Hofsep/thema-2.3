4d7e1d8e6947425914bb48ad7e30828fa55c4125
Khanji <ferdi.sancak@gmail.com>
2015-03-25 20:36:25 +0100

    yuppi

--- Leertaak4MobileRobot/src/Vraag1/NumberPrinter.java
+++ Leertaak4MobileRobot/src/Vraag1/NumberPrinter.java
@@ -2,98 +2,163 @@ package Vraag1;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
-public class NumberPrinter {
-	
+/**
+ * NumberPrinter
+ * creates and executes the threads
+ * 
+ * @author Ferdi
+ *
+ */
+public class NumberPrinter
+{	
+	/**
+	 * main
+	 * autoloader
+	 * @param args
+	 */
 	public static void main(String[] args)
 	{
-		ExecutorService executor = Executors.newFixedThreadPool(4);
+		ExecutorService executor = Executors.newCachedThreadPool();
 		executor.execute(new PrintNumber(1));
 		executor.execute(new PrintNumber(2));
 		executor.execute(new PrintNumber(3));
 		executor.execute(new PrintNumber(4));
 		
-		while(!executor.isTerminated())
-		{
-
-		}
+		//When done, shut down
 		executor.shutdown();
-
 	}
 	
+
+	/**
+	 * PrintNumber
+	 * Internal class that implements Runnable
+	 * this is the actual thread that prints the number.
+	 * 
+	 * @author Ferdi
+	 *
+	 */
 	public static class PrintNumber implements Runnable
 	{
+		/**
+		 * private class variables
+		 */
 		private int number;
-		private static Num num = new Num();
+		private static Lock lock = new ReentrantLock();
+		private static Condition isReady = lock.newCondition();
+		private static Number num = new Number();
 		
-		public PrintNumber(int i)
+		/**
+		 * constructor
+		 * @param i - number
+		 */
+		PrintNumber(int i)
 		{
 			number = i;
-			System.out.println(number);
-		}
-
-		@Override
-		public void run() 
-		{
-			try {
-	            while (true) {
-	                putMessage();
-	            }
-	        } catch (InterruptedException e) {
-	        }
 		}
 		
-		private synchronized void putMessage() throws InterruptedException {
+		@Override
+	    public void run()
+	    {
+			/* Print it */ 
+		 // print(); 
+			
+			/* Print it reverse */ 
+		 // /*
+			try
+	        {
+		        reversePrint();
+	        }
+	        catch (InterruptedException e)
+	        {
+		        // TODO Auto-generated catch block
+		        e.printStackTrace();
+	        } //*/
+		    
+	    }
+		/**
+		 * print number twice
+		 */
+		private void print()
+	    {
+			lock.lock();
 			try
 			{
-				if (num.getNumber() != number) {
-		            wait();
-		        }
-				num.substract();
-				for(int i = 0; i < 2; i++)
-				{
-					System.out.print(print());
-				}
-				System.out.print(" ");
-				System.out.print(num.getNumber()+" ");
-				notify();
+			    for(int i = 0;i < 2; i++)
+			    {
+			    	System.out.print(number);
+			    }
+			    System.out.print(" ");
 			}
 			finally
 			{
-				notify();
+				lock.unlock();
+			}
+		    
+	    }
+		
+		/**
+		 * print number twice in reverse order
+		 * @throws InterruptedException
+		 */
+		private void reversePrint() throws InterruptedException
+		{
+			lock.lock();
+			try
+			{
+				while(number < num.num)
+				{
+					isReady.await();
+				}
+				num.subNum();
+				for(int i = 0;i < 2; i++)
+			    {
+			    	System.out.print(number);
+			    }
+			    System.out.print(" ");
+			    isReady.signalAll();
+				
+			}
+			finally
+			{
+				lock.unlock();
 			}
 		}
-
-		private int print()
-		{
-				return number;
-		}
-	}
-	
-	public static class Num
-	{
-		private int numb = 4;
 		
-		public Num()
+		
+	}
+
+	/**
+	 * Class Number
+	 * Internal class to keep track of what number 
+	 * is in the thread to determine whether the 
+	 * thread needs to wait or be executed
+	 * 
+	 * @author Ferdi
+	 *
+	 */
+	static class Number
+	{
+		int num = 4;
+		
+		/**
+		 * empty constructor
+		 */
+		Number()
 		{
 			
 		}
 		
-		public void setNumber(int i)
+		/**
+		 * substract 1 of num
+		 */
+		void subNum()
 		{
-			numb = i;
+			if(num > 0)
+			num -=1;
 		}
-		
-		public int getNumber()
-		{
-			return numb;
-		}
-		
-		public void substract()
-		{
-			numb -= 1;
-		}
-		
 	}
-	
 }
